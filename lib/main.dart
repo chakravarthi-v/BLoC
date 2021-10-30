17f177ca8cf05f9c98e670606402eaa2567dd715
chakravarthi-v <chakravarthiviswanath@gmail.com>
2021-10-30 23:05:29 +0530

    BlocProvider Implementation

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timer/cubit/counter_cubit.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'BLoC',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("pushed"),
+            BlocBuilder<CounterCubit,CounterState>(
+              builder: (context, state) {
+                if(state.counterValue<0){
+                  return Text("Ahmmm"+state.counterValue.toString());
+                }
+                return Text(state.counterValue.toString());
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FloatingActionButton(onPressed: () {
+                  BlocProvider.of<CounterCubit>(context).decrement();
+                },
+                  tooltip: "Decrement",
+                  child: const Icon(Icons.remove),
+                ),
+                FloatingActionButton(onPressed: () {
+                  BlocProvider.of<CounterCubit>(context).increment();
+                },
+                  tooltip: "Increment",
+                  child: const Icon(Icons.add),),
+
+              ],
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
